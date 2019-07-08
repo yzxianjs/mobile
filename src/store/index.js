c03a66fdf28ce80708335767ec7501f9cf59adc4
yzxianjs <2638497222@11.com>
2019-07-08 13:15:16 +0800

    仓库初始化

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import userModule from '@/store/modules/userModule';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+	state : {},
+	mutations : {},
+	actions : {},
+	modules : {
+		userModule,
+	},
+});
